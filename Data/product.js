749958bae635a763aa711396ec657a7ccf6daf6a
Your Name <you@example.com>
2025-01-16 13:59:18 +0100

    save

---
+++ Data/product.js
@@ -0,0 +1,57 @@
+const ourWork = [
+  {
+    image: "Images/bg61.jpeg",
+    title: "Technicians",
+  },
+  {
+    image: "Images/bg10.jpeg",
+    title: "AC Workshop",
+  },
+  {
+    image: "Images/AC img3.jpeg",
+    title: "AC Diagnosis",
+  },
+  {
+    image: "Images/AC img2.jpeg",
+    title: "AC Testing",
+  },
+];
+
+
+const sevices = [
+  {
+    image: "Images/AC img12.jpeg",
+    title: "AC Shop",
+  },
+  {
+    image: "Images/AC img19.jpeg",
+    title: "AC Condenser",
+  },
+  {
+    image: "Images/AC img18.jpeg",
+    title: "AC Compressor",
+  },
+  {
+    image: "Images/gas-charging-valve.jpeg",
+    title: "AC Gas",
+  },
+];
+
+const datas = [
+  {
+    image: "Images/AC img2.jpeg",
+    title: "Work Completed",
+  },
+  {
+    image: "Images/AC img1.jpeg",
+    title: "AC Testing",
+  },
+  {
+    image: "Images/AC img9.jpeg",
+    title: "Kudos AC is Working",
+  },
+  {
+    image: "Images/bg1.jpeg",
+    title: "Customer is Leaving...",
+  },
+];
--- Jscript/app.js
+++ Jscript/app.js
@@ -3,6 +3,34 @@ const mnl = document.querySelector(".mobile-nav-links");
 const heda = document.querySelector("header");
 const form = document.querySelector("form");
 let back2Top = document.querySelector(".back-to-top");
+let back2TopContainer = document.querySelector(".back-to-top-container");
+let navContainer = document.querySelector(".nav-container");
+let li = document.querySelectorAll(".li-nav-link");
+
+
+ for (let i = 0; i < li.length; i++) {
+   li[i].addEventListener("click", function () {
+     mnl.classList.remove("h-[350px]");
+   });
+ }
+    
+
+
+window.addEventListener("scroll", () => {
+  scrollY > 145
+    ? navContainer.classList.add("fixed")
+    : navContainer.classList.remove("fixed");
+});
+
+
+
+
+window.addEventListener('scroll', () => {
+  scrollY > 1000 
+  ? back2TopContainer.classList.remove('hidden')  
+  : back2TopContainer.classList.add("hidden");
+  
+})
 
 function closeMobileNav() {
  setTimeout(() => {
@@ -11,6 +39,7 @@ function closeMobileNav() {
 }
 mnb.addEventListener('click', () =>{
   mnl.classList.toggle('h-[350px]')
+  
 })
 heda.addEventListener("mouseleave", closeMobileNav);
 
@@ -23,6 +52,56 @@ form.addEventListener('submit', (r)=>{
 function b2t() {
   scrollTo({ top: 0, behavior: "smooth" });
 
-
 }
 back2Top.addEventListener("click", b2t);
+
+
+
+ourWork.forEach((data) =>{
+  let eachProduct = document.querySelector(".what-we-do");
+
+
+
+  eachProduct.innerHTML += `
+          <div class="w-[17rem] grid place-items-center">
+            <img class="w-[10rem] lg:w-[17rem] h-[8] lg:h-[10rem]" src="${data.image}" alt="">
+            <h2 class="py-2 font-black text-center">${data.title}</h2>
+          </div>
+  
+  `;
+})
+sevices.forEach((data) => {
+  let eachProduct = document.querySelector(".Other-services");
+
+  eachProduct.innerHTML += `
+          <div class="w-[17rem] grid place-items-center">
+            <img class="w-[10rem] lg:w-[17rem] h-[8] lg:h-[10rem]" src="${data.image}" alt="">
+            <h2 class="py-2 font-black text-center">${data.title}</h2>
+          </div>
+  
+  `;
+});
+
+for( const data of datas) {
+  let eachProduct = document.querySelector(".projects");
+  eachProduct.innerHTML += `
+          <div class="w-[17rem] grid place-items-center">
+            <img class="w-[10rem] lg:w-[17rem] h-[8] lg:h-[10rem]" src="${data.image}" alt="">
+            <h2 class="py-2 font-black text-center">${data.title}</h2>
+          </div> 
+  `;
+};
+
+/*
+projects.forEach((data) => {
+  let eachProduct = document.querySelector(".projects");
+
+  eachProduct.innerHTML += `
+          <div class="w-[17rem] grid place-items-center">
+            <img class="w-[10rem] lg:w-[17rem] h-[8] lg:h-[10rem]" src="${data.image}" alt="">
+            <h2 class="py-2 font-black text-center">${data.title}</h2>
+          </div>
+  
+  `;
+});
+*/
